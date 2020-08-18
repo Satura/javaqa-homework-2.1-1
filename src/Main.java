e116f7e8ab8242abb241b9d7f3ce72c17b5433cb
Anastasia Sv <selen500@mail.ru>
2020-08-18 17:28:21 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 64739;
+        int miles = ticketPrice/20;
+
+        System.out.println("Начислено милей: " + miles);
+    }
+}
